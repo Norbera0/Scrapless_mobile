c4e844e3196f1ca156e97cbe705aa5f9c6a7d24f
norbor <nribrea@up.edu.ph>
2025-08-18 13:42:07 +0000

    in dont let them go to waste, replace the image and use the image locate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -289,7 +289,7 @@ export default function DashboardPage() {
                 </div>
                 <div className="relative w-1/3 flex-shrink-0">
                     <Image
-                        src="/dashboard/recipe_generator_home.png"
+                        src="/dashboard/recipe_generator_home_2.png"
                         alt="Recipe suggestions illustration"
                         layout="fill"
                         objectFit="cover"
