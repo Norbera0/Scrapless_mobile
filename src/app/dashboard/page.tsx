089a8396dea2c6e08629cdd870f29f263bc88ddb
norbor <nribrea@up.edu.ph>
2025-08-18 19:01:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -231,7 +231,7 @@ export default function DashboardPage() {
       <div className="relative p-5 space-y-6">
 
           <Card className="shadow-lg text-white relative overflow-hidden rounded-2xl">
-            <Image src="/dashboard/user_impact_final.jpg" layout="fill" objectFit="cover" alt="Impact background" className="z-0" />
+            <Image src="/bpi-logo-2.png" layout="fill" objectFit="cover" alt="Impact background" className="z-0" />
             <div className="absolute inset-0 bg-black/40 z-0"></div>
             <div className="relative z-10 p-6 space-y-4">
               <CardHeader className="p-0">
